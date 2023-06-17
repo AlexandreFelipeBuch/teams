22411cd04f820c4e4fad9e370ae655bafcc9681b
Alexandre Felipe Moreira Iargan Buch <felipe.m.buch@gmail.com>
2023-06-17 14:47:44 -0300

    Componente Header e HighLight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,12 @@
+import { HighLight } from "@components/HighLight";
 import { Container } from "./styles";
+import { Header } from "@components/Header";
 
 export function Groups() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+      <HighLight title="Turmas" subtitle=" Jogue com a sua turma" />
+    </Container>
+  );
 }
