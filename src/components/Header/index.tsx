399fd9ea7215f04eac4546dea1c50dd19b8afbb2
Alexandre Felipe Moreira Iargan Buch <felipe.m.buch@gmail.com>
2023-06-19 14:03:27 -0300

    Componente GroupCard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import { Container, Logo, BackButton, BackIcon } from "./styles";
 import logoImg from "@assets/logo.png";
+type Props = {
+  showBackButton?: boolean;
+};
 
-export function Header() {
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
-      <BackButton>
-        <BackIcon />
-      </BackButton>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   );
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
-import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <HighLight title="Turmas" subtitle=" Jogue com a sua turma" />
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
